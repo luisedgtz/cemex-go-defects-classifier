affd7193c9fe662a3ca3145d1c95473cce2b46d0
Luis <luiseduardo.gtz@outlook.com>
2022-05-23 16:41:11 -0500

    Get description data

--- src/screens/Classifier.js
+++ src/screens/Classifier.js
@@ -13,48 +13,6 @@ const Classifier = () => {
     const [columns, setColumns] = useState([]);
     const [data, setData] = useState([]);
 
-    const processData = dataString => {
-        console.log(dataString)
-        // const dataStringLines = dataString.split(/\r\n|\n/);
-        // console.log(dataStringLines[2])
-        // console.log(dataStringLines[0])
-        // const headers = dataStringLines[0].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
-
-     
-        // const list = [];
-        // for (let i = 1; i < dataStringLines.length; i++) {
-        //     const row = dataStringLines[i].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
-        //     if (headers && row.length == headers.length) {
-        //         const obj = {};
-        //         for (let j = 0; j < headers.length; j++) {
-        //             let d = row[j];
-        //             if (d.length > 0) {
-        //                 if (d[0] == '"')
-        //                     d = d.substring(1, d.length - 1);
-        //                 if (d[d.length - 1] == '"')
-        //                     d = d.substring(d.length - 2, 1);
-        //             }
-        //             if (headers[j]) {
-        //                 obj[headers[j]] = d;
-        //             }
-        //         }
-     
-        //         if (Object.values(obj).filter(x => x).length > 0) {
-        //             list.push(obj);
-        //         }
-        //     }
-        // }
-     
-        // prepare columns list from headers
-        // const columns = headers.map(c => ({
-        //   name: c,
-        //   selector: c,
-        // }));
-     
-        // setData(list);
-        // setColumns(columns);
-    }
-
     const handleChange = (event) => {
         setClusters(event.target.value);
     };
@@ -74,7 +32,8 @@ const Classifier = () => {
             const ws = wb.Sheets[wsname];
             /* Convert array of arrays */
             const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
-            processData(data);
+            let description = data.map(a => a[10]);
+            console.log(description)
         };
         reader.readAsBinaryString(uploadedFile);
     }
