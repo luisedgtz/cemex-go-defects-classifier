5d33fdefa4544a2d40ed5445cc3f1cb92a30abd2
Luis Eduardo Gutiérrez Saenz Pardo <luiseduardo.gtz@outlook.com>
2022-05-26 16:18:15 -0500

    Update Classifier.js

--- src/screens/Classifier.js
+++ src/screens/Classifier.js
@@ -39,12 +39,7 @@ const Classifier = () => {
             const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
             let description = data.map(a => a[10]);
             
-            
-            
-            const items = description.slice(0, 50)
-
-
-            setDefects(items);
+            setDefects(description.slice(1,description.length));
             setData(data);
 
             //console.log(description)
@@ -59,7 +54,6 @@ const Classifier = () => {
         try{
             const response = await axios.post(`${baseUrl}/reports/script`, { 
                 "defects_array": defects
-                
             })
             if (response.status === 200) {
                 console.log(response.data)
@@ -91,24 +85,21 @@ const Classifier = () => {
                         )}
                         </Dropzone>
                     </Box>
-                    <Box sx={{mt: 3, alignItems: "center", justifyContent: "space-between", width: "60%", display: "flex", flexDirection: "row"}}>
+
+                    <Typography sx={{mt: 2}}>{defects.length} defects identified</Typography>
+
+                    <Box sx={{mt: 1, alignItems: "center", justifyContent: "space-between", width: "60%", display: "flex", flexDirection: "row"}}>
                         <Box display="flex" alignItems="center">
                             <InputLabel htmlFor='clusters' color='text'>Number of groups</InputLabel>
                             <TextField
+                                type="number"
                                 id="clusters"
-                                select
                                 label=""
                                 value={clusters}
                                 onChange={handleChange}
-                                sx={{ml: 2}}
-                            >
-                                    <MenuItem key={1} value={1}>1</MenuItem>
-                                    <MenuItem key={2} value={2}>2</MenuItem>
-                                    <MenuItem key={3} value={3}>3</MenuItem>
-                                    <MenuItem key={4} value={4}>4</MenuItem>
-                                    <MenuItem key={5} value={5}>5</MenuItem>
-                                    <MenuItem key={6} value={6}>6</MenuItem>
-                            </TextField>
+                                inputProps={{max: defects.length, min: 1}}
+                                sx={{ml: 2, width: "30%"}}
+                            />
                         </Box>
 
                         <Button sx={{margin: 0}} color="secondary" variant="contained" onClick={handleProcess}>Process</Button>
